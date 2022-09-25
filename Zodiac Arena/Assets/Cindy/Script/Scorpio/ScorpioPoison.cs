daedbaba862236d317e570934f76fed7d1486b18
cindyoshita <cindynat11@gmail.com>
2022-09-25 17:58:09 -0300

    ScorpioPoison - ver 1.0
    - Comentado o código
    - Tirado alguns debugs

--- Zodiac Arena/Assets/Cindy/Script/Scorpio/ScorpioPoison.cs
+++ Zodiac Arena/Assets/Cindy/Script/Scorpio/ScorpioPoison.cs
@@ -6,21 +6,25 @@ public class ScorpioPoison : MonoBehaviour
     public float speedX = 5f;
     public float speedY = 5f;
     public Rigidbody2D rb;
+    
     void Start()
     {
         PosionMove(rb,speedX, speedY);
     }
+    
+    // Criação/Instanciação do gameobject veneno
     public void Shoot(Transform poisonPoint, GameObject poisonPrefab )
     {
         Instantiate(poisonPrefab, poisonPoint.position, poisonPoint.rotation);
     }
 
+    // Movimentação em segundo grau do veneno
     public void PosionMove(Rigidbody2D rb, float speed, float speed2)
     {
         rb.velocity = transform.right * speed + transform.up * speed2;
-        Debug.Log(rb.velocity);
     }
 
+    // Quando entrar em contato com qualquer gameObject diferente da Scorpio, o veneno é destruido
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name != "Scorpio") 
